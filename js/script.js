ce40588f8e4a98d74a00096e6e8fe8bb9dbaf3ce
albertoBarl <alberto.barletta00@gmail.com>
2022-12-06 16:11:20 +0100

    fixed css, added problem js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle per ognuna delle 10 righe. Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata. 
+// Ogni cella ha un numero progressivo, da 1 a 100. 
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// 1.Creare un collegamento col bottone "Play"
+// 2.Generare una griglia 10x10 collegata la bottone "Play"
+// 3."Numerare" ogni cella
+// 3.Al click di una cella, essa si colora di azzurro, resituendo il numero di cella in console
+
+
+
