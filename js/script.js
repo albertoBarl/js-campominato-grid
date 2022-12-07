0d9c995d362fd63589a8817198c26b03741e3afa
albertoBarl <alberto.barletta00@gmail.com>
2022-12-07 09:49:23 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -9,51 +9,59 @@
 // 3.Al click di una cella, essa si colora di azzurro, resituendo il numero di cella in console
 
 let play_button = document.getElementById('play-button');
-let level = document.getElementById('Livello')
+let level = document.getElementById('Livello');
 const container = document.querySelector('div.c-square');
 
 play_button.addEventListener('click', function(){
+    // LIVELLO 3, GRIGLIA 10X10
     if(level.value == 'Level_3'){
         for(let i=1; i<=100; i++){
             let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red3', 'd-flex', 'justify-content-center', 'align-items-center')
+            square.classList.add('sq', 'sq-red3', 'd-flex', 'justify-content-center', 'align-items-center');
             container.appendChild(square);
             
+            // funzione 'click' a griglia creata
             square.addEventListener('click', function(){
-                this.classList.add('sq-aqua', 'text-dark')
-                this.innerText= i
-                console.log(this.innerText)
+                this.classList.add('sq-aqua', 'text-dark');
+                this.innerText= i;
+                console.log(this.innerText);
             })
         }
     }
+
+    // LIVELLO 2, GRIGLIA 9X9
     else if(level.value == 'Level_2'){
         for(let i=1; i<=81; i++){
             let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red2', 'd-flex', 'justify-content-center', 'align-items-center')
+            square.classList.add('sq', 'sq-red2', 'd-flex', 'justify-content-center', 'align-items-center');
             container.appendChild(square);
             
+            // funzione 'click' a griglia creata
             square.addEventListener('click', function(){
-                this.classList.add('sq-aqua', 'text-dark')
-                this.innerText= i
-                console.log(this.innerText)
+                this.classList.add('sq-aqua', 'text-dark');
+                this.innerText= i;
+                console.log(this.innerText);
             })
         }
     }
+
+    // LIVELLO 1, GRIGLIA 7X7
     else if(level.value == 'Level_1'){
         for(let i=1; i<=49; i++){
             let square= document.createElement('button');
-            square.classList.add('sq', 'sq-red1', 'd-flex', 'justify-content-center', 'align-items-center')
+            square.classList.add('sq', 'sq-red1', 'd-flex', 'justify-content-center', 'align-items-center');
             container.appendChild(square);
             
+            // funzione 'click' a griglia creata
             square.addEventListener('click', function(){
-                this.classList.add('sq-aqua', 'text-dark')
-                this.innerText= i
-                console.log(this.innerText)
+                this.classList.add('sq-aqua', 'text-dark');
+                this.innerText= i;
+                console.log(this.innerText);
             })
         }
     }
     else{
-        alert('Scegli un livello di difficoltà')
+        alert('Scegli un livello di difficoltà');
     }
 })
 
